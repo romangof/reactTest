afab681d240d9c61fb44a7cc63043f98705b4f53
Roman Fuentes <roman@MBPDeRoman.local>
2017-03-14 13:45:12 -0300

    Moved navbar and ohers to components, added indexroutes to make it default

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import ReactRouter, {Router, Route, IndexRoute, hashHistory, browserHistory} from 'react-router';
-// import Section from './Section';
-// import Top from './Navbar';
-// import Ss1 from './home/Section1';
-// import Ss2 from './home/Section2';
-// import Ss3 from './home/Section3';
-import Home from './home/home';
+import Main from './components/Main';
+import Home from './components/home/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-          <Route path='/' component={Home} />
+          <Route path='/' component={Main}>
+            <IndexRoute component={Home} />
+            <Route path='/asd' component={Home} />
+          </Route>
       </Router>
     );
   }
