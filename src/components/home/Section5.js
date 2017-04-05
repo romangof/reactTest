c53e4c1d37e1752d0c5f8093c726a9eefd5ca948
karensonia <arancibia.karen@gmail.com>
2017-04-05 17:28:02 -0300

    Adding images

--- src/components/home/Section5.js
+++ src/components/home/Section5.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import {Col} from 'react-bootstrap';
+import Img1 from '../../assets/antofagastaminerals.png'
+import Img2 from '../../assets/bci.png'
+import Img3 from '../../assets/enaex.png'
+import Img4 from '../../assets/collahuasi.png'
+import Img5 from '../../assets/consorcio.png'
+import Img6 from '../../assets/800doctor.png'
+import Img7 from '../../assets/entel.png'
+import Img8 from '../../assets/arysta.png'
+import Img9 from '../../assets/gobiernodechile.png'
+import Img10 from '../../assets/scotiabank.png'
+import {Col, Thumbnail} from 'react-bootstrap';
+
 
 var sectionStyle = {
-    height: '50vh',
+    height: '80vh',
     color: 'rgb(242, 160, 50)',
     padding: '5% 0%',
 }
 
 export default () =>  
 <section style={sectionStyle}>
-    loopThis
+    <h3>
+        <b>CONFÍAN EN NOSOTROS</b>
+    </h3>
     <div style={{paddingTop: '5%', color: 'gray'}}>
-        <Col xs={6} sm={2} smOffset={1}>Logo Img 1</Col>
-        <Col xs={6} sm={2}>Logo Img 2</Col>
-        <Col xs={6} sm={2}>Logo Img 3</Col>
-        <Col xs={6} sm={2}>Logo Img 4</Col>
-        <Col xs={6} sm={2}>Logo Img 5</Col>
-        <Col xs={6} sm={2} smOffset={1}>Logo Img 6</Col>
-        <Col xs={6} sm={2}>Logo Img 7</Col>
-        <Col xs={6} sm={2}>Logo Img 8</Col>
+        <Col xs={6} sm={2} smOffset={1}><Thumbnail src={Img1} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img2} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img3} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img4} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img5} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2} smOffset={1}><Thumbnail src={Img6} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img7} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img8} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img9} alt="10x10"></Thumbnail></Col>
+        <Col xs={6} sm={2}><Thumbnail src={Img10} alt="10x10"></Thumbnail></Col>
+
     </div>
 </section>
