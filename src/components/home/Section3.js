2eb268b9c3c05f56883ff40a77fa6e27d4dab328
Roman Fuentes <romangof@gmail.com>
2017-04-07 18:26:23 -0300

    Not showing carousel when there is only 1 item to display

--- src/components/home/Section3.js
+++ src/components/home/Section3.js
@@ -54,20 +54,53 @@ var Ss3 = React.createClass({
         position: 'Subgerente de Marketing, Enaex',
         comment: '"Nos han ayudado a darle un foco distinto a nuestra marca y también a hacer más eficiente los procesos de desarrollo de gestión de imagen, siempre con una actitud y disponibilidad inmediata. Estamos muy contentos por los resultados obtenidos gracias al compromiso fuerte que tienen por prestar un servicio de excelencia".',
         link: ''
-      }, {
-        id: 2,
-        name: '',
-        position: '',
-        comment: '',
-        link: ''
-      }, {
-        id: 3,
-        name: '',
-        position: '',
-        comment: '',
-        link: ''
       }
+    //   }, {
+    //     id: 2,
+    //     name: '',
+    //     position: '',
+    //     comment: '',
+    //     link: ''
+    //   }, {
+    //     id: 3,
+    //     name: '',
+    //     position: '',
+    //     comment: '',
+    //     link: ''
+    //   }
     ];
+
+    const single = (
+      <div style={carouselStyle}>
+        <p><b>{testimonials[0].name}</b></p>
+        <p>{testimonials[0].position}</p>
+        <br/>
+        <p>{testimonials[0].comment}</p>
+        <button className="homebutton1">VER CASO DE ÉXITO</button>
+      </div>)
+    
+    const carousel = (
+      <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
+        {testimonials.map((element) => 
+          <Carousel.Item key={element.id}>
+            <div style={carouselStyle}>
+              <p><b>{element.name}</b></p>
+              <p>{element.position}</p>
+              <br/>
+              <p>{element.comment}</p>
+            </div>
+            {/*<img width={"100%"} height={"70vh"} src="/assets/carousel.png"/>*/}
+            <Carousel.Caption>
+              {/*<h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>*/}
+              <button className="homebutton1">VER CASO DE ÉXITO</button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )}
+      </Carousel>)
+
+
+            
 
     return (
       <section className="container-fluid" style={sectionStyle}>
@@ -75,26 +108,8 @@ var Ss3 = React.createClass({
           <h4 className="hometitle2"><b>TESTIMONIOS</b></h4>
         </Col>        
         <div>
-          <Carousel activeIndex={this.state.index} direction={this.state.direction} onSelect={this.handleSelect}>
-
-            {testimonials.map((element) => 
-              <Carousel.Item key={element.id}>
-                <div style={carouselStyle}>
-                  <p><b>{element.name}</b></p>
-                  <p>{element.position}</p>
-                  <br/>
-                  <p>{element.comment}</p>
-                </div>
-                {/*<img width={"100%"} height={"70vh"} src="/assets/carousel.png"/>*/}
-                <Carousel.Caption>
-                  {/*<h3>First slide label</h3>
-                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>*/}
-                  <button className="homebutton1">VER CASO DE ÉXITO</button>
-                </Carousel.Caption>
-              </Carousel.Item>
-            )}
-
-          </Carousel>
+          {console.log(testimonials.length)}
+          {(testimonials.length < 2) ? single:carousel}
         </div>
       </section>
     );
