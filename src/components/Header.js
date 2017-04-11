9cdbfe71703562b0d3d18656e112d2a006d04d83
Roman Fuentes <romangof@gmail.com>
2017-04-11 17:46:07 -0300

    Header made into a class to add dropdown hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,49 +3,71 @@ import logo from '../assets/logo_header.png';
 import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 
-export default (props) => 
-<Navbar collapseOnSelect fixedTop className={(props.location !== '/') ? 'navbarElse' : 'navbarIndex'}>
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isOpen: false }
+  }
 
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="/">
-        <img src={logo} alt="Aceleracion Labs."/>
-      </a>
-    </Navbar.Brand>
-    <Navbar.Toggle/>
-  </Navbar.Header>
+  handleOpen = () => {
+    this.setState({ isOpen: true })
+  }
 
-  <Navbar.Collapse>
-    <Nav pullRight>
-      <IndexLinkContainer to="/">
-        <NavItem>INICIO</NavItem>
-      </IndexLinkContainer>
-      <LinkContainer to="/who">
-        <NavItem>SOMOS</NavItem>
-      </LinkContainer>
-      <LinkContainer to='/how' onClick={(event) => event.preventDefault()} >
-        <NavDropdown className="dropdown" title="TE AYUDAMOS" id="basic-nav-dropdown">
-          <LinkContainer to='/how/comunicaciones'>
-            <MenuItem className="dropdown-content">COMUNICACIONES Y MARKETING DIGITAL</MenuItem>
-          </LinkContainer>
-          <LinkContainer to='/how/diseño'>
-            <MenuItem className="dropdown-content">DISEÑO E IDENTIDAD CORPORATIVA</MenuItem>
-          </LinkContainer>
-          <LinkContainer to='/how/ti'>
-            <MenuItem className="dropdown-content">SOLUCIONES TI</MenuItem>
-          </LinkContainer>
-          <LinkContainer to='/how/negocios'>
-            <MenuItem className="dropdown-content">MODELOS DE NEGOCIOS</MenuItem>
-          </LinkContainer>
-        </NavDropdown>
-      </LinkContainer>
-      {/*<LinkContainer to="/">*/}
-        {/*<NavItem>BLOG</NavItem>*/}
-      {/*</LinkContainer>*/}
-      <LinkContainer to="/contact">
-        <NavItem>CONTACTO</NavItem>
-      </LinkContainer>
-    </Nav>
-  </Navbar.Collapse>
+  handleClose = () => {
+     this.setState({ isOpen: false })
+  }
 
-</Navbar>;
+  render (props) {
+    return (
+      <Navbar collapseOnSelect fixedTop className={(location.pathname !== '/') ? 'navbarElse' : 'navbarIndex'}>
+
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">
+              <img src={logo} alt="Aceleracion Labs."/>
+            </a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <IndexLinkContainer to="/">
+              <NavItem>INICIO</NavItem>
+            </IndexLinkContainer>
+            <LinkContainer to="/who">
+              <NavItem>SOMOS</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/how' onClick={(event) => event.preventDefault()} >
+              <NavDropdown onMouseEnter={ this.handleOpen }
+                onMouseLeave={ this.handleClose }
+                open={ this.state.isOpen } noCaret
+                className="dropdown" title="TE AYUDAMOS" id="basic-nav-dropdown">
+                <LinkContainer to='/how/comunicaciones'>
+                  <MenuItem className="dropdown-content">COMUNICACIONES Y MARKETING DIGITAL</MenuItem>
+                </LinkContainer>
+                <LinkContainer to='/how/diseño'>
+                  <MenuItem className="dropdown-content">DISEÑO E IDENTIDAD CORPORATIVA</MenuItem>
+                </LinkContainer>
+                <LinkContainer to='/how/ti'>
+                  <MenuItem className="dropdown-content">SOLUCIONES TI</MenuItem>
+                </LinkContainer>
+                <LinkContainer to='/how/negocios'>
+                  <MenuItem className="dropdown-content">MODELOS DE NEGOCIOS</MenuItem>
+                </LinkContainer>
+              </NavDropdown>
+            </LinkContainer>
+            {/*<LinkContainer to="/">*/}
+              {/*<NavItem>BLOG</NavItem>*/}
+            {/*</LinkContainer>*/}
+            <LinkContainer to="/contact">
+              <NavItem>CONTACTO</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Navbar>
+    )
+  }  
+};
+
