c3e1ca58ec1c371806bc2df7e4a9917d60262497
karensonia <arancibia.karen@gmail.com>
2017-04-13 15:56:43 -0300

    thank you page update (2)

--- src/components/thanks/Thanks.js
+++ src/components/thanks/Thanks.js
@@ -13,7 +13,7 @@ var sectionStyles = {
 
 export default () => 
 <div>
-<section style={sectionStyles}>
+<section style={sectionStyles} >
           <Col >
             <Thumbnail  src={Img} responsive ></Thumbnail>
                 <h3 style={{color: '#00A69C'}}><b>¡Gracias por escribirnos, <br/>pronto estaremos conversando!
@@ -21,11 +21,5 @@ export default () =>
               </Col>
 
     <br/>
-    {/*<p>Un grupo de empresas que trabajan en conjunto para ofrecer soluciones disruptivas de negocios a través de diseño, estrategias digitales e innovación tecnológica.</p>
-    <br/>
-    <p>Nos especializamos en comunicación estratégica, marketing digital, diseño gráfico, identidad corporativa, desarrollo de software, aplicaciones móviles y sitios web.</p>
-    <br/>
-    <p>Brindamos una oferta de valor integral que responde a las necesidades específicas de nuestros clientes con formas innovadoras de hacer negocios desde la perspectiva del software, diseño y comunicación.</p>*/}
-
 </section>;
 </div>
