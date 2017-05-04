9b52e2bddf11c76d3713de8b5ebb6c715a1a253d
karensonia <arancibia.karen@gmail.com>
2017-05-04 18:00:01 -0300

    instagram new button -footer-

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import Img from '../assets/facebook.png'
+import Img2 from '../assets/twitter.png'
+import Img3 from '../assets/linkedin.png'
+import Img4 from '../assets/instagram.png'
+import { Thumbnail } from 'react-bootstrap';
+
 // import { Image } from 'react-bootstrap';
 
 var sectionStyle = {
@@ -12,16 +18,13 @@ var sectionStyle = {
 export default () => 
 <section style={sectionStyle} >
   <br/>
-  <div style={{paddingLeft: '4%'}} >
-    <a href="https://www.facebook.com/aceleracionlabs/">
-      <div className={'btn btn-link socialmedia facebookbutton'} />
-    </a>
-    <a href="https://twitter.com/AceleracionLabs">
-      <div className={'btn btn-link socialmedia twitterbutton'} />
-    </a>
-    <a href="https://www.linkedin.com/company-beta/10178890/">
-      <div className={'btn btn-link socialmedia linkedinbutton'} />
-    </a>
+  <div  className="socialmedia">
+  <ul style={{paddingLeft: '4%'}} >
+<li><a href="https://www.facebook.com/aceleracionlabs/"><img src={Img} /></a></li>
+<li><a href="https://twitter.com/AceleracionLabs"><img src={Img2} /></a></li>
+<li><a href="https://www.linkedin.com/company-beta/10178890/"><img src={Img3} /></a></li>
+<li><a href="https://www.instagram.com/team.aceleracion/"><img src={Img4} /></a></li>
+  </ul>
   </div>
   <br/>
   <b>ACELERACIÓN LABS</b>
