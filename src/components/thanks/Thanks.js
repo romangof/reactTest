8153cea6e89d15f0b60f2978d7f551e333655a1f
arancibia.karen@gmail.com <arancibia.karen@gmail.com>
2017-09-04 17:26:41 -0300

    margin disclaimer

--- src/components/thanks/Thanks.js
+++ src/components/thanks/Thanks.js
@@ -81,7 +81,7 @@ export default (props) => {
               <b>{ (route !== 'contacto') ? 'CONTÁCTANOS':'DESCARGAR' }</b>
             </button>
           </a>
-          <p>Tus datos serán recogidos con el fin único de enviarte la información solicitada. Tus datos están protegidos y son confidenciales.</p>
+          <p style={{marginTop: '50px'}}>Tus datos serán recogidos con el fin único de enviarte la información solicitada. Tus datos están protegidos y son confidenciales.</p>
         </div>}
     </Col>
     <br/>
