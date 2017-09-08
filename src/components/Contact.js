5890da40f153d1a9c885fecf8369cdd82459b640
Roman Fuentes <romangof@gmail.com>
2017-09-08 12:18:09 -0300

    Updated react packages and changed contact form to wufoo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,121 +3,140 @@ import { Col, Image, FormControl, FormGroup, ControlLabel, Button } from 'react-
 import { browserHistory } from 'react-router';
 import runtimeEnv from '@mars/heroku-js-runtime-env'
 import { Helmet } from "react-helmet";
+import WufooForm from "react-wufoo-embed";
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super();
-    this.state = { data: {} };
-    this.handleChange = this.handleChange.bind(this);
-    this.post = this.post.bind(this);
-  }
+export default () => <div>
+  <Helmet>
+    <title>Contáctanos | Aceleración Labs</title>
+    <meta name="Description" content="Hablemos, nos encantaría ayudarte a alcanzar los objetivos comerciales de tu empresa." />
+  </Helmet>
+  <section className={'sectionsHow'} style={{textAlign: 'left', overflow: 'hidden', height: '100%'}}>
 
-  handleChange(e) {
-    var data = this.state.data;
-    data[e.target.name] = e.target.value;
-    this.setState({data: data});
-  }
+    <Col xs={6} xsOffset={3} sm={4} smOffset={1} style={{padding: '5%'}}>
+      <Image src={require('../assets/contact.png')} responsive/>
+    </Col>
 
-  post (e) {
-    e.preventDefault();
-    const env = runtimeEnv();
-    var data = this.state.data;
-    if (!data.option) data.option = this.props.params.from || 'comunicaciones';
-    if (data.name && data.company && data.job_title && data.email && data.telephone && data.message) {
-      // fetch(`https://aceleracion.herokuapp.com/api/contacts`, { // use local, comment when pushing to heroku
-      fetch(`${env.REACT_APP_API_URL}/contacts`, { // comment local, use when pushing to heroku
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({contact: this.state.data})
-      })
-      .then((response) => {
-        if(response.ok) {
-          var image = new Image(1, 1); 
-          image.src = `//www.googleadservices.com/pagead/conversion/${873644984}/?label=${'qzlyCPCZinMQuIfLoAM'}&script=0`;
-          browserHistory.push(`gracias/${ (data.option !== 'newsletter') ? 'contacto/'+data.option:data.option }`);
-        } else {
-          return response.json();
-        }
-      })
-      .then((response) => {response && console.log(response.error)})
-      .catch((error) => {console.log(error)})
-    }
-  }
+    <Col xs={12} sm={5} smOffset={1} style={{padding: '5%'}}>
+      <WufooForm userName="aceleracion" formHash="sc7ibw11g1pnc7"  />
+    </Col>
+  </section>
+</div>
 
-  render () {
-    return (
-      <div>
-        <Helmet>
-          <title>Contáctanos | Aceleración Labs</title>
-          <meta name="Description" content="Hablemos, nos encantaría ayudarte a alcanzar los objetivos comerciales de tu empresa." />
-        </Helmet>
-        <section className={'sectionsHow'} style={{textAlign: 'left', overflow: 'hidden', height: '100%'}}>
 
-          <Col xs={6} xsOffset={3} sm={4} smOffset={1} style={{padding: '5%'}}>
-            <Image src={require('../assets/contact.png')} responsive/>
-          </Col>
+// export default class Contact extends React.Component {
+//   constructor(props) {
+//     super();
+//     this.state = { data: {} };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.post = this.post.bind(this);
+//   }
 
-          <Col xs={12} sm={5} smOffset={1} style={{padding: '5%'}}>
-            <h4 style={{color: '#00A69C'}}><b>CONTACTO</b></h4>
-            <br/>Hablemos, nos encantaría ayudarte.
-            <br/>
-            <br/>
+//   handleChange(e) {
+//     var data = this.state.data;
+//     data[e.target.name] = e.target.value;
+//     this.setState({data: data});
+//   }
 
-            <form onSubmit={this.post} id="gform_7">
-              <FormGroup controlId="formName" validationState={(this.state.data.name) ? 'success':'error'}>
-                <ControlLabel>Nombre</ControlLabel>
-                <FormControl type="text" name='name' onChange={this.handleChange} required/>
-              </FormGroup>
+//   post (e) {
+//     e.preventDefault();
+//     const env = runtimeEnv();
+//     var data = this.state.data;
+//     if (!data.option) data.option = this.props.params.from || 'comunicaciones';
+//     if (data.name && data.company && data.job_title && data.email && data.telephone && data.message) {
+//       // fetch(`https://aceleracion.herokuapp.com/api/contacts`, { // use local, comment when pushing to heroku
+//       fetch(`${env.REACT_APP_API_URL}/contacts`, { // comment local, use when pushing to heroku
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         method: 'POST',
+//         body: JSON.stringify({contact: this.state.data})
+//       })
+//       .then((response) => {
+//         if(response.ok) {
+//           var image = new Image(1, 1); 
+//           image.src = `//www.googleadservices.com/pagead/conversion/${873644984}/?label=${'qzlyCPCZinMQuIfLoAM'}&script=0`;
+//           browserHistory.push(`gracias/${ (data.option !== 'newsletter') ? 'contacto/'+data.option:data.option }`);
+//         } else {
+//           return response.json();
+//         }
+//       })
+//       .then((response) => {response && console.log(response.error)})
+//       .catch((error) => {console.log(error)})
+//     }
+//   }
 
-              <FormGroup controlId="formCompany" validationState={(this.state.data.company) ? 'success':'error'}>
-                <ControlLabel>Empresa</ControlLabel>
-                <FormControl type="text" name='company' onChange={this.handleChange} required/>
-              </FormGroup>
+//   render () {
+//     return (
+//       <div>
+//         <Helmet>
+//           <title>Contáctanos | Aceleración Labs</title>
+//           <meta name="Description" content="Hablemos, nos encantaría ayudarte a alcanzar los objetivos comerciales de tu empresa." />
+//         </Helmet>
+//         <section className={'sectionsHow'} style={{textAlign: 'left', overflow: 'hidden', height: '100%'}}>
+
+//           <Col xs={6} xsOffset={3} sm={4} smOffset={1} style={{padding: '5%'}}>
+//             <Image src={require('../assets/contact.png')} responsive/>
+//           </Col>
+
+//           <Col xs={12} sm={5} smOffset={1} style={{padding: '5%'}}>
+//             <h4 style={{color: '#00A69C'}}><b>CONTACTO</b></h4>
+//             <br/>Hablemos, nos encantaría ayudarte.
+//             <br/>
+//             <br/>
+
+//             <form onSubmit={this.post} id="gform_7">
+//               <FormGroup controlId="formName" validationState={(this.state.data.name) ? 'success':'error'}>
+//                 <ControlLabel>Nombre</ControlLabel>
+//                 <FormControl type="text" name='name' onChange={this.handleChange} required/>
+//               </FormGroup>
+
+//               <FormGroup controlId="formCompany" validationState={(this.state.data.company) ? 'success':'error'}>
+//                 <ControlLabel>Empresa</ControlLabel>
+//                 <FormControl type="text" name='company' onChange={this.handleChange} required/>
+//               </FormGroup>
               
-              <FormGroup controlId="formTitle" validationState={(this.state.data.job_title) ? 'success':'error'}>
-                <ControlLabel>Cargo</ControlLabel>
-                <FormControl type="text" name='job_title' onChange={this.handleChange} required/>
-              </FormGroup>
+//               <FormGroup controlId="formTitle" validationState={(this.state.data.job_title) ? 'success':'error'}>
+//                 <ControlLabel>Cargo</ControlLabel>
+//                 <FormControl type="text" name='job_title' onChange={this.handleChange} required/>
+//               </FormGroup>
 
-              <FormGroup controlId="formMail" validationState={/^\S+@\S+\.\S+$/.test(this.state.data.email) ? 'success':'error'}>
-                <ControlLabel>Mail</ControlLabel>
-                <FormControl type="email" name='email' onChange={this.handleChange} required/>
-              </FormGroup>
+//               <FormGroup controlId="formMail" validationState={/^\S+@\S+\.\S+$/.test(this.state.data.email) ? 'success':'error'}>
+//                 <ControlLabel>Mail</ControlLabel>
+//                 <FormControl type="email" name='email' onChange={this.handleChange} required/>
+//               </FormGroup>
 
-              <FormGroup controlId="formPhone" validationState={/\(?([0-9]{3})\)?([ .-]?)([0-9]{4})\2([0-9]{4})/.test(this.state.data.telephone) ? 'success':'error'}>
-                <ControlLabel>Teléfono</ControlLabel>
-                <FormControl type="text" name='telephone' onChange={this.handleChange} required/>
-              </FormGroup>
+//               <FormGroup controlId="formPhone" validationState={/\(?([0-9]{3})\)?([ .-]?)([0-9]{4})\2([0-9]{4})/.test(this.state.data.telephone) ? 'success':'error'}>
+//                 <ControlLabel>Teléfono</ControlLabel>
+//                 <FormControl type="text" name='telephone' onChange={this.handleChange} required/>
+//               </FormGroup>
               
-              <FormGroup controlId="formInterest">
-                <ControlLabel>Interés</ControlLabel>
-                <FormControl componentClass="select" name='option' defaultValue={this.props.params.from || 'comunicaciones'} onChange={this.handleChange} >
-                  <option value="comunicaciones">Marketing y Comunicaciones</option>
-                  <option value="diseno">Diseño e identidad corporativa</option>
-                  <option value="ti">Soluciones TI</option>
-                  <option value="negocios">Negocios</option>
-                  <option value="newsletter">Suscripcion</option>
-                </FormControl>
-              </FormGroup>
+//               <FormGroup controlId="formInterest">
+//                 <ControlLabel>Interés</ControlLabel>
+//                 <FormControl componentClass="select" name='option' defaultValue={this.props.params.from || 'comunicaciones'} onChange={this.handleChange} >
+//                   <option value="comunicaciones">Marketing y Comunicaciones</option>
+//                   <option value="diseno">Diseño e identidad corporativa</option>
+//                   <option value="ti">Soluciones TI</option>
+//                   <option value="negocios">Negocios</option>
+//                   <option value="newsletter">Suscripcion</option>
+//                 </FormControl>
+//               </FormGroup>
 
-              <FormGroup controlId="formComment" validationState={(this.state.data.message) ? 'success':'error'}>
-                <ControlLabel>Comentario</ControlLabel>
-                <FormControl componentClass="textarea" style={{height: '200px'}} name='message' onChange={this.handleChange} required/>
-              </FormGroup>
+//               <FormGroup controlId="formComment" validationState={(this.state.data.message) ? 'success':'error'}>
+//                 <ControlLabel>Comentario</ControlLabel>
+//                 <FormControl componentClass="textarea" style={{height: '200px'}} name='message' onChange={this.handleChange} required/>
+//               </FormGroup>
 
-              <Button onClick={this.post} type='submit' bsSize="large" style={{
-                color: 'white',
-                backgroundColor: '#871D5F',
-                borderRadius: 0,
-                padding: '2vh 4vh 2vh 4vh'}}>Enviar</Button>
+//               <Button onClick={this.post} type='submit' bsSize="large" style={{
+//                 color: 'white',
+//                 backgroundColor: '#871D5F',
+//                 borderRadius: 0,
+//                 padding: '2vh 4vh 2vh 4vh'}}>Enviar</Button>
 
-            </form>
-          </Col>
-        </section>
-      </div>
-    );
-  }
-}
+//             </form>
+//           </Col>
+//         </section>
+//       </div>
+//     );
+//   }
+// }
